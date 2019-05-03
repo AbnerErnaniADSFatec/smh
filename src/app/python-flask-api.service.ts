d64557bb8d0a8aecf98e59ce96a1c46889ef5e0c
Abner Anjos <33687984+AbnerErnaniADSFatec@users.noreply.github.com>
2019-05-03 16:39:56 -0300

    com merge monthly e city

--- src/app/python-flask-api.service.ts
+++ src/app/python-flask-api.service.ts
@@ -4,6 +4,10 @@ import { Pcd } from './map/points/pcd';
 import { PcdHistory } from './map/points/pcd-history';
 import { Geotiff } from './map/rasters/geotiff';
 import { AnaliseGeotiff } from './map/rasters/analise-geotiff';
+import { CityByState } from './map/entities/city-by-state';
+import { State } from './map/entities/state';
+import { StateUnique } from './map/entities/state-unique';
+import { CityByStateUnique } from './map/entities/city-by-state-unique';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -59,5 +63,31 @@ export class PythonFlaskAPIService{
 
   getMergeMonthlyMaxMean(geocodigo: string){
     return this.httpClient.get<AnaliseGeotiff>(this.localhost + '/an_merge_monthly/' + geocodigo);
+  }
+
+  getCities(uf: string){
+    return this.httpClient.get<CityByState>(this.localhost + '/cities/' + uf);
+  }
+
+  getStates(){
+    return this.httpClient.get<State>(this.localhost + '/states');
+  }
+
+  convertToArray(obj: Object){
+    let vetor = [];
+    for( let i = 0; obj[i] != null; i++ ){ vetor[i] = obj[i]; }
+    return vetor;
+  }
+
+  convertToCityAPI(nome1: string[], geocodigo: string[] ){
+    let cities: CityByStateUnique[] = [];
+    for( let i in nome1 ){ cities[i] = { nome1 : nome1[i], geocodigo : geocodigo[i] }; }
+    return cities;
+  }
+
+  convertToStateAPI(estado: string[], uf: string[]){
+    let states: StateUnique[] = [];
+    for( let i in estado ){ states[i] = { estado : estado[i], uf : uf[i] }; }
+    return states;
   }
 }
